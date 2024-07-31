ae8c42873e5ddbdb71057846bd637df81535ca62
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-07-31 20:40:37 +0530

    Add files via upload

---
+++ .github/sql_uploads/snowflake_queries.sql
@@ -0,0 +1,11 @@
+/*ALTER TABLE TEST_DB.PUBLIC.EMPLOYEE ADD COLUMN FLAG varchar(2);
+ALTER TABLE TEST_DB.PUBLIC.EMPLOYEE DROP COLUMN CONTACT;
+
+insert into TEST_DB.PUBLIC.EMPLOYEE values ('abc','456','Y');
+
+insert into TEST_DB.PUBLIC.EMPLOYEE values ('def','789','Y');*/
+
+
+--insert into TEST_DB.PUBLIC.EMPLOYEE values ('ghi','789','Y');
+
+insert into TEST_DB.PUBLIC.EMPLOYEE values ('jkl','789','Y');
