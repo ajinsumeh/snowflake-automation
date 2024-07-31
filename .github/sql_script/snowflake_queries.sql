dd14ce1f03158fb5a2b05c9e06d2899e27491df4
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-07-31 22:50:24 +0530

    Update snowflake_queries.sql

--- .github/sql_script/snowflake_queries.sql
+++ .github/sql_script/snowflake_queries.sql
@@ -1,3 +1,6 @@
 
 
-insert into TEST_DB.PUBLIC.EMPLOYEE values ('ghi','789','Y');
+--insert into TEST_DB.PUBLIC.EMPLOYEE values ('ghi','789','Y');
+
+insert into TEST_DB.PUBLIC.EMPLOYEE values ('jkl','789','Y');
+
