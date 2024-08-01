f8833f693d8876e92267e5905a0dace5d0979953
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-08-01 14:16:00 +0530

    Add files via upload

--- .github/sql_uploads/snowflake_queries.sql
+++ .github/sql_uploads/snowflake_queries.sql
@@ -17,4 +17,6 @@ insert into TEST_DB.PUBLIC.EMPLOYEE values ('def','789','Y');*/
 
 --insert into TEST_DB.PUBLIC.EMPLOYEE values ('stu','789','Y','abcd');
 
-insert into TEST_DB.PUBLIC.EMPLOYEE values ('uvw','789','Y','abcd');
+--insert into TEST_DB.PUBLIC.EMPLOYEE values ('uvw','789','Y','abcd');
+
+alter table TEST_DB.PUBLIC.EMPLOYEE drop column dummy;
