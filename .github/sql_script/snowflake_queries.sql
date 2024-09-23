adab9e0b30102b4656cc4b09cd88fa06a2d53908
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-09-23 13:22:30 +0530

    Add files via upload

--- .github/sql_script/snowflake_queries.sql
+++ .github/sql_script/snowflake_queries.sql
@@ -1,6 +1,23 @@
+/*ALTER TABLE TEST_DB.PUBLIC.EMPLOYEE ADD COLUMN FLAG varchar(2);
+ALTER TABLE TEST_DB.PUBLIC.EMPLOYEE DROP COLUMN CONTACT;
+
+insert into TEST_DB.PUBLIC.EMPLOYEE values ('abc','456','Y');
+
+insert into TEST_DB.PUBLIC.EMPLOYEE values ('def','789','Y');*/
 
 
 --insert into TEST_DB.PUBLIC.EMPLOYEE values ('ghi','789','Y');
 
-insert into TEST_DB.PUBLIC.EMPLOYEE values ('jkl','789','Y');
+--insert into TEST_DB.PUBLIC.EMPLOYEE values ('jkl','789','Y');
+--insert into TEST_DB.PUBLIC.EMPLOYEE values ('mno','789','Y');
 
+--insert into TEST_DB.PUBLIC.EMPLOYEE values ('pqrs','789','Y');
+
+--alter table TEST_DB.PUBLIC.EMPLOYEE add column dummy varchar(15);
+
+--insert into TEST_DB.PUBLIC.EMPLOYEE values ('stu','789','Y','abcd');
+
+--insert into TEST_DB.PUBLIC.EMPLOYEE values ('uvw','789','Y','abcd');
+
+
+insert into TEST_DB.PUBLIC.test_employee_2 values ('Ajin','23456','Y');
