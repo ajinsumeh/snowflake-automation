8eadf37275ef25c60a40a41bfd5bbad333a85c0a
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-09-23 16:16:23 +0530

    Add files via upload

--- .github/sql_script/snowflake_queries.sql
+++ .github/sql_script/snowflake_queries.sql
@@ -5,19 +5,6 @@ insert into TEST_DB.PUBLIC.EMPLOYEE values ('abc','456','Y');
 
 insert into TEST_DB.PUBLIC.EMPLOYEE values ('def','789','Y');*/
 
+--insert into TEST_DB.PUBLIC.test_employee_2 values ('Ajin','23456','Y');
 
---insert into TEST_DB.PUBLIC.EMPLOYEE values ('ghi','789','Y');
-
---insert into TEST_DB.PUBLIC.EMPLOYEE values ('jkl','789','Y');
---insert into TEST_DB.PUBLIC.EMPLOYEE values ('mno','789','Y');
-
---insert into TEST_DB.PUBLIC.EMPLOYEE values ('pqrs','789','Y');
-
---alter table TEST_DB.PUBLIC.EMPLOYEE add column dummy varchar(15);
-
---insert into TEST_DB.PUBLIC.EMPLOYEE values ('stu','789','Y','abcd');
-
---insert into TEST_DB.PUBLIC.EMPLOYEE values ('uvw','789','Y','abcd');
-
-
-insert into TEST_DB.PUBLIC.test_employee_2 values ('Ajin','23456','Y');
+insert into TEST_DB.PUBLIC.test_employee_2 values ('test1','123','Y');
