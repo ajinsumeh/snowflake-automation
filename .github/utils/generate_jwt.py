6b1e6808a028dfcf26dc4c8b0219ab2776226416
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-11-28 19:52:57 +0530

    Update generate_jwt.py

--- .github/utils/generate_jwt.py
+++ .github/utils/generate_jwt.py
@@ -1,28 +1,35 @@
+import sys
 import time
 import jwt
-from jose import jwt as jose_jwt
 
 # Load private key from file
-private_key = open('.github/private/prreviewajin2024.2024-11-28.private-key.pem', 'r').read()
+
+with open('.github/private/prreviewajin2024.2024-11-28.private-key.pem', 'rb') as pem_file:
+    private_key = pem_file.read()
+
 print(private_key)
 # GitHub App ID
 app_id = '1072194'  # Replace with your actual GitHub App ID
-
+client_id='Iv23liIBLbPfqF3jlZX3'
 # JWT payload
 jwt_payload = {
-    "iat": int(time.time()),  # Issued at time
-    "exp": int(time.time()) + 600,  # Expiration time (10 minutes)
-    "iss": app_id  # GitHub App ID
+    # Issued at time
+    'iat': int(time.time()),
+    # JWT expiration time (10 minutes maximum)
+    'exp': int(time.time()) + 600,
+    
+    # GitHub App's client ID
+    'iss': client_id
 }
 
-# Generate JWT token
-jwt_token = jwt.encode(jwt_payload, private_key, algorithm="RS256")
+encoded_jwt = jwt.encode(payload, signing_key, algorithm='RS256')
+
+print(f"JWT: {encoded_jwt}")
 
 # Save JWT token to a file
 with open(".github/secrets/jwt_token.txt", "w") as file:
-    file.write(jwt_token)
-
-# Print JWT token to the workflow logs
-print(f"JWT Token: {jwt_token}")
+    file.write(encoded_jwt)
 
 print("JWT token generated and saved.")
+
+
