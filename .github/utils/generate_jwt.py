e1a6b2f2daf07f7ded2df1dba5682baa696c1a67
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-11-28 19:59:25 +0530

    Update generate_jwt.py

--- .github/utils/generate_jwt.py
+++ .github/utils/generate_jwt.py
@@ -3,14 +3,13 @@ import time
 import jwt
 
 # Load private key from file
-
 with open('.github/private/prreviewajin2024.2024-11-28.private-key.pem', 'rb') as pem_file:
     private_key = pem_file.read()
 
-print(private_key)
-# GitHub App ID
+# GitHub App ID and Client ID
 app_id = '1072194'  # Replace with your actual GitHub App ID
-client_id='Iv23liIBLbPfqF3jlZX3'
+client_id = 'Iv23liIBLbPfqF3jlZX3'  # Replace with your actual GitHub App client ID
+
 # JWT payload
 jwt_payload = {
     # Issued at time
@@ -22,7 +21,8 @@ jwt_payload = {
     'iss': client_id
 }
 
-encoded_jwt = jwt.encode(payload, signing_key, algorithm='RS256')
+# Encode JWT using private key
+encoded_jwt = jwt.encode(jwt_payload, private_key, algorithm='RS256')
 
 print(f"JWT: {encoded_jwt}")
 
@@ -31,5 +31,3 @@ with open(".github/secrets/jwt_token.txt", "w") as file:
     file.write(encoded_jwt)
 
 print("JWT token generated and saved.")
-
-
