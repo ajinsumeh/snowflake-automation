6f7b78e5659835fe80cae84b470806a62b21f635
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-12-10 17:41:44 +0530

    Add files via upload (#72)

---
+++ .github/sql_script/b_sql.sql
@@ -0,0 +1,9 @@
+--hello I am going to automate github snowflake automation case 2
+
+create or replace table TEST_DATABASE.GIT_FETCH.EMPLOYEE1(id int, name varchar(20));
+
+-- This is data row 1
+insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE1 values (1,'Ajin');
+
+-- This is data row 2
+insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE1 values (2,'Arya');
