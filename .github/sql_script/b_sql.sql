aa3027b2e760fee397094609c3d386c5babc3524
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-12-10 17:44:04 +0530

    Delete .github/sql_script/b_sql.sql

--- .github/sql_script/b_sql.sql
+++
@@ -1,9 +0,0 @@
---hello I am going to automate github snowflake automation case 2
-
-create or replace table TEST_DATABASE.GIT_FETCH.EMPLOYEE1(id int, name varchar(20));
-
--- This is data row 1
-insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE1 values (1,'Ajin');
-
--- This is data row 2
-insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE1 values (2,'Arya');
