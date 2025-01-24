e301604b68c5f37dca93f3874af3e7132dc1c348
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2025-01-24 16:23:23 +0530

    Add files via upload

--- .github/sql_script/a_sql.sql
+++ .github/sql_script/a_sql.sql
@@ -1,4 +1,8 @@
 --Creating a table
 
+/*create or replace TABLE TEST_DATABASE.GIT_FETCH.EMPLOYEE (
+	ID NUMBER(38,0),
+	NAME VARCHAR(20)
+);*/
 
-insert into abc;
+insert into TABLE TEST_DATABASE.GIT_FETCH.EMPLOYEE values (1,'Ajin');
