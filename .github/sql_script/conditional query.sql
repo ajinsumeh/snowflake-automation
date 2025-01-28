cd14d8d1421000f26a4c3180078be934c96a5e99
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2025-01-28 18:09:06 +0530

    Add files via upload

---
+++ .github/sql_script/conditional query.sql
@@ -0,0 +1,5 @@
+--hello I am going to automate github snowflake automation case 2
+
+-- This is data row 3
+insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE1 values (3,'C');
+CREATE or replace table TEST_DATABASE.GIT_FETCH.EMPLOYEE1(id int, name varchar(20));
