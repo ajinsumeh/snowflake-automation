b89d661fe2bbe7c69caa80e38bdcaf618451808b
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2025-02-09 16:22:04 +0530

    Delete .github/sql_script/conditional_execution.sql

--- .github/sql_script/conditional_execution.sql
+++
@@ -1,6 +0,0 @@
---hello I am going to automate github snowflake automation case 2
-
--- This is data row 3
-insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (3,'C');
-
-CREATE or replace table TEST_DATABASE.GIT_FETCH.EMPLOYEE1(id int, name varchar(20));
